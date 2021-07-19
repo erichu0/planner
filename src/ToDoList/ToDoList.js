5653fc3517a7ef2cf22c8b2305c689e4a3e81e2e
erictheeric <ehu5591@bths.edu>
2021-07-19 00:10:04 -0400

    set and get firebase finally work

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 
 import './ToDoList.css';
 
@@ -68,70 +68,84 @@ const ToDoList = () => {
             console.error(error);
         });
 
+        database.ref('doneToDos').get().then((snapshot) => {
+            console.log(snapshot.val());
+            if (snapshot.exists()) {
+                setDoneToDos(snapshot.val());
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
     }
 
-    const saveLocalTodos = () => {
-        localStorage.setItem('toDos', JSON.stringify(toDos));
-        localStorage.setItem('doneToDos', JSON.stringify(doneToDos));
-    }
+    // const saveLocalTodos = () => {
+    //     localStorage.setItem('toDos', JSON.stringify(toDos));
+    //     localStorage.setItem('doneToDos', JSON.stringify(doneToDos));
+    // }
 
-    const getLocalTodos = () => {
-        if (localStorage.getItem('toDos') === null) {
-            localStorage.setItem('toDos', JSON.stringify([]));
-        } else {
-            setToDos(JSON.parse(localStorage.getItem('toDos', JSON.stringify(toDos))));
-            //setToDos(["waiting for firebase", "still waiting"]);
-        };
-        if (localStorage.getItem('doneToDos') === null) {
-            localStorage.setItem('doneToDos', JSON.stringify([]));
-        } else {
-            setDoneToDos(JSON.parse(localStorage.getItem('doneToDos', JSON.stringify(doneToDos))));
-            //setDoneToDos(["waiting for firebase", "still waiting"]);
-        };
-    }
+    // const getLocalTodos = () => {
+    //     if (localStorage.getItem('toDos') === null) {
+    //         localStorage.setItem('toDos', JSON.stringify([]));
+    //     } else {
+    //         setToDos(JSON.parse(localStorage.getItem('toDos', JSON.stringify(toDos))));
+    //         //setToDos(["waiting for firebase", "still waiting"]);
+    //     };
+    //     if (localStorage.getItem('doneToDos') === null) {
+    //         localStorage.setItem('doneToDos', JSON.stringify([]));
+    //     } else {
+    //         setDoneToDos(JSON.parse(localStorage.getItem('doneToDos', JSON.stringify(doneToDos))));
+    //         //setDoneToDos(["waiting for firebase", "still waiting"]);
+    //     };
+    // }
 
     useEffect(() => {
         getFirebase();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    const didMount = useRef(false);
+
     useEffect(() => {
-        setTimeout(setFirebase(), 2000);
-        saveLocalTodos();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        if (didMount.current) {
+            setFirebase()
+            //saveLocalTodos();
+        } else didMount.current = true;
+        // eslint-disable-next-line
     }, [deletedCount, toDos, doneToDos])
 
-    return (
-        <div className='to-do-list'>
-            <h1 className="w1 title">To Do List</h1>
-            <div className="input">
-                <Input
-                    inputText={inputText}
-                    setInputText={setInputText}
-                    toDos={toDos}
-                    setToDos={setToDos} />
-            </div>
-            <div className="task-list">
-                <TaskList
-                    inputText={inputText}
-                    toDos={toDos}
-                    setToDos={setToDos}
-                    setDoneToDos={setDoneToDos}
-                    doneToDos={doneToDos}
-                    deletedCount={deletedCount}
-                    setDeletedCount={setDeletedCount} />
-            </div>
-            <div className="done">
-                <Done
-                    doneToDos={doneToDos}
-                    setDoneToDos={setDoneToDos}
-                    toDos={toDos}
-                    deletedCount={deletedCount}
-                    setDeletedCount={setDeletedCount} />
-            </div>
-
+return (
+    <div className='to-do-list'>
+        <h1 className="w1 title">To Do List</h1>
+        <div className="input">
+            <Input
+                inputText={inputText}
+                setInputText={setInputText}
+                toDos={toDos}
+                setToDos={setToDos} />
         </div>
-    )
+        <div className="task-list">
+            <TaskList
+                inputText={inputText}
+                toDos={toDos}
+                setToDos={setToDos}
+                setDoneToDos={setDoneToDos}
+                doneToDos={doneToDos}
+                deletedCount={deletedCount}
+                setDeletedCount={setDeletedCount} />
+        </div>
+        <div className="done">
+            <Done
+                doneToDos={doneToDos}
+                setDoneToDos={setDoneToDos}
+                toDos={toDos}
+                deletedCount={deletedCount}
+                setDeletedCount={setDeletedCount} />
+        </div>
+
+    </div>
+)
 }
 
 export default ToDoList;
