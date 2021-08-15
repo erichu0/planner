1d0c108606d66fe1efb8dc61aeba43ddef90235b
erictheeric <ehu5591@bths.edu>
2021-08-14 23:10:24 -0400

    shift stuff to sleep input

--- src/SleepTracker/SleepTracker.js
+++ src/SleepTracker/SleepTracker.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SleepChart from './SleepChart';
+import SleepInput from './SleepInput'
 
 const SleepTracker = () => {
 
@@ -79,30 +80,16 @@ const SleepTracker = () => {
             }
         }
     };
-    const timeHandler = (e) => {
-        e.preventDefault();
-        setTime([...time, new Date().getMinutes()]);
-    }
-
-    const sleepHandler = (e) => {
-        e.preventDefault();
-        times.shift()
-        times.push([1, 3]);
-        console.log(times);
-        data.datasets.data = times;
-    }
-
-    const wakeUpHandler = (e) => {
-        e.preventDefault();
-    }
 
     return (
         <div className='bg-black'>
-            <form>
-                <input type='text'></input>
-                <button className='bg-yellow-500' onClick={sleepHandler}>Sleep Now</button>
-                <button className='bg-yellow-500' onClick={wakeUpHandler}>Wake up Now</button>
-            </form>
+            <SleepInput 
+                time={time}
+                setTime={setTime}
+                times={times}
+                data={data}
+                options={options}
+            />
             <div className='signup w3 p-4'>
                 <h1 className='font-bold text-2xl mb-4'>Sleep Tracker</h1>
             </div>
